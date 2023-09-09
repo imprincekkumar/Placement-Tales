c5261be36a118e35b78c6e757118e990e55ea6bb
imprincekkumar <80305084+imprincekkumar@users.noreply.github.com>
2023-09-09 17:21:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,13 @@ app.use(morgan("dev"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/blog", require("./routes/blogRoutes"));
 
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 // Port
 const PORT = process.env.PORT || 8080;
