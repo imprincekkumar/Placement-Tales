b655fb7ff09d54540f282b2e09447e00f533ccc3
imprincekkumar <80305084+imprincekkumar@users.noreply.github.com>
2023-09-09 17:12:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ dotenv.config();
 
 // router import
 
-const userRoutes = require("./routes/userRoutes");
-const blogRoutes = require("./routes/blogRoutes");
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/blog", require("./routes/blogRoutes"));
 
 
 
